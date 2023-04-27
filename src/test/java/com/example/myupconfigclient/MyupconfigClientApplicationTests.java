4189077f29ec4e4f23163a3b6e24a4e5006e0eb7
YenPu Chen <yenpu.upyoung@gmail.com>
2023-04-27 14:07:56 +0800

    Update

--- src/test/java/com/example/myupconfigclient/MyupconfigClientApplicationTests.java
+++ src/test/java/com/example/myupconfigclient/MyupconfigClientApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.myupconfigclient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class MyupconfigClientApplicationTests {
 
     @Test
